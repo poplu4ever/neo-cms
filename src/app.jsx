49a9e28c115bb7310326eae5e9bc54c16e25bc0a
jiexinglu <lujiexing66@sina.com>
2019-03-28 15:49:17 +1100

    通用部分组件开发

--- src/app.jsx
+++ src/app.jsx
@@ -17,7 +17,11 @@ class App extends React.Component{
                     <Layout>
                         <Switch>
                             <Route exact path="/" component={Home} />
-                            <Redirect from = "*" to = "/"/>
+                            <Route path="/product" component={Home}/>
+                            <Route path="/product-category" component={Home}/>
+                            <Route path="/order" component={Home}/>
+                            <Route path="/user" component={Home}/>
+                            {/*<Redirect from = "*" to = "/"/>*/}
                         </Switch>
                     </Layout>
                 </Router>
--- src/page/home/index.jsx
+++ src/page/home/index.jsx
@@ -1,12 +1,16 @@
 import React, { Component } from 'react';
-
+import PageTitle from '../../component/page-title/index.jsx';
 import './index.css';
 
 class Home extends Component{
+    constructor(props){
+        super(props);
+    }
+
     render(){
         return(
-            <div>
-                <i className="fas fa-igloo"></i>
+            <div id='page-wrapper'>
+                <PageTitle title='首页'/>
                 <button className='btn btn-default'>Test App</button>
             </div>
         )
