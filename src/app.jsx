493ce74366c627844c99b5ebe08238861a2d221b
jiexinglu <lujiexing66@sina.com>
2019-05-10 18:28:03 +1000

    order modules

--- src/app.jsx
+++ src/app.jsx
@@ -7,6 +7,8 @@ import Home             from './page/home/index.jsx';
 import Layout           from './component/layout/index.jsx';
 import Login            from './page/login/index.jsx';
 import Error            from './page/error/index.jsx';
+import OrderList        from './page/order/index.jsx';
+import OrderDetail      from './page/order/detail.jsx';
 import UserList         from './page/userList/index.jsx';
 import ProductRouter    from './page/product/router.jsx';
 
@@ -24,9 +26,11 @@ class App extends React.Component{
                     <Route exact path="/" component={Home} />
                     <Route path="/product" component={ProductRouter}/>
                     <Route path="/product-category" component={ProductRouter}/>
-                    <Route path="/order" component={Home}/>
+                    <Route path="/order/index" component={OrderList}/>
+                    <Route path="/order/detail/:orderNumber" component={OrderDetail}/>
                     <Route path="/user/index" component={UserList}/>
                     <Redirect exact from='/user' to="/user/index"/>
+                    <Redirect exact from='/order' to="/order/index"/>
                     <Route component={Error}/>
                 </Switch>
             </Layout>);
